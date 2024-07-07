cdb8c34ecef85fba0323d3acc4281d46d88dc499
SchrOdinger11 <sudhanshu.kulkarni2105@gmail.com>
2024-07-06 23:36:32 -0400

    added basic routes and setup project structure

--- touristhelper/src/App.js
+++ touristhelper/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router , Route, Redirect, Switch} from 'react-router-dom/cjs/react-router-dom.min';
+import User from './user/pages/user';
+import Places from './places/pages/places';
+//first component rendered so routing is set up here
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+  <Switch>
+  {/* //Route can be used only inside router */}
+<Route path ="/" exact> 
+<User/>
+</Route>
+
+
+<Route path ="/places/new" exact> 
+<Places/>
+</Route>
+
+<Redirect to="/"></Redirect>
+</Switch>
+</Router>
+
+
   );
 }
 
